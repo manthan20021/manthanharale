08c2aacf7367fac1dadb5baa68bdacfbae99022d
Manthan harale <126886816+manthan20021@users.noreply.github.com>
2025-08-28 09:28:18 -0700

    disable the Turbppack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    turbo: false, // disable Turbopack
+  },
+};
 
 export default nextConfig;
